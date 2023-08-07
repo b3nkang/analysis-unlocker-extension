e045fb46c88ab36760e38d20159e2d0ad0806f9a
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-06 18:43:36 -0700

    IP update

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message) => {
   const url = message.url;
-  fetch('http://127.0.0.1:5002/', {
+  fetch('http://0.0.0.0:5002/', {
     method: 'POST',
     body: JSON.stringify({ url }),
     headers: {
