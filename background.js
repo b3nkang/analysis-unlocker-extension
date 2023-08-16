5e4ac2312885fa18250e51ee70b260f06da420a3
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-16 00:07:56 -0700

    v0.2.7 build: downgrade VM IP changes, prep for redeployment to prod

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message) => {
   const url = message.url;
-  fetch('http://34.86.32.88/', {
+  fetch('http://34.145.134.242/', {
     method: 'POST',
     body: JSON.stringify({ url }),
     headers: {
